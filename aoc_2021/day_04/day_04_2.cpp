e972f6ca4784d1b1fbf52a2bb1259c0f009b2c1f
Jul-Le <julius.lesonen@gmail.com>
2021-12-06 17:26:17 +0200

    Day 4 part 2

---
+++ aoc_2021/day_04/day_04_2.cpp
@@ -0,0 +1,224 @@
+/*
+ *  Contributor(s):     JulLe
+ *
+ *  Created:            2021-12-06
+ *  Modified:           2021-12-06
+ *
+ *
+ *
+ *  Find the board that is going to find last.
+ *
+ *  What is the sum of the unmarked numbers multiplied my value of the number
+ *  that was marked last?
+ */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstring>
+
+class BingoBoard
+{
+public:
+    BingoBoard() { }
+    BingoBoard(std::vector<std::vector<int>> n)
+        : m_numbers(n)
+    {
+        for (int x = 0; x < 5; x++)
+            m_marked.push_back({ 0,0,0,0,0 });
+        m_win = false;
+    }
+    
+    BingoBoard(const BingoBoard &b)
+    {
+        this->m_numbers = b.m_numbers;
+        this->m_marked = b.m_marked;
+    }
+    
+    void mark(int num)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (m_numbers[i][j] == num)
+                {
+                    m_marked[i][j] = true;
+                    return;
+                }
+            }
+        }
+    }
+    
+    bool win()
+    {
+        // If board has won once, m_win will be set to true so the
+        // following checks don't have to be executed every time
+        if (m_win)
+            return true;
+        
+        for (int i = 0; i < 5; i++)
+        {
+            int marked = 0;
+            
+            for (int j = 0; j < 5; j++)
+            {
+                if (m_marked[i][j])
+                    marked++;
+                else break;
+            }
+            
+            if (marked == 5)
+            {
+                m_win = true;
+                return true;
+            }
+            
+            marked = 0;
+            
+            for (int j = 0; j < 5; j++)
+            {
+                if (m_marked[j][i])
+                    marked++;
+                else break;
+            }
+            
+            if (marked == 5)
+            {
+                m_win = true;
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    int unmarkedSum()
+    {
+        int sum = 0;
+        
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (!m_marked[i][j])
+                    sum += m_numbers[i][j];
+            }
+        }
+        
+        return sum;
+    }
+    
+private:
+    std::vector<std::vector<int>> m_numbers;
+    std::vector<std::vector<bool>> m_marked;
+    bool m_win;
+};
+
+std::vector<std::string> parseInput()
+{
+    std::ifstream file;
+    std::string line;
+    std::vector<std::string> data;
+    
+    file.open("input.txt");
+    
+    if (!file.is_open())
+    {
+        perror("Failed to open file \"input.txt\"");
+        exit(EXIT_FAILURE);
+    }
+    
+    while (getline(file, line))
+    {
+        data.push_back(line);
+    }
+    
+    file.close();
+    
+    return data;
+}
+
+std::vector<int> getNumbers(std::string s)
+{
+    std::vector<int> nums;
+    
+    char *token = strtok((char*)s.c_str(), ",");
+    
+    while (token)
+    {
+        nums.push_back(std::atoi(token));
+        
+        token = strtok(NULL, ",");
+    }
+    
+    return nums;
+}
+
+int main()
+{
+    std::vector<std::string> input = parseInput();
+    std::vector<int> numbers = getNumbers(input[0]);
+    std::vector<BingoBoard*> boards;
+    
+    // First row contains numbers, second row is blank so start from 3th
+    for (int i = 2; i < input.size(); i++)
+    {
+        std::string str = input[i];
+        
+        if (str == "")
+            continue;
+        
+        std::vector<std::vector<int>> numbers;
+        int a, b, c, d, e;
+        
+        for (int j = i; j < i+5; j++)
+        {
+            str = input[j];
+            sscanf(str.c_str(), "%d %d %d %d %d", &a, &b, &c, &d, &e);
+            numbers.push_back({ a, b, c, d, e });
+        }
+        
+        boards.push_back(new BingoBoard(numbers));
+        i += 5;
+    }
+    
+    BingoBoard *losingBoard;
+    int lastNum;
+    const int boardCount = boards.size();
+    int boardsLeft = boardCount;
+    
+    for (auto num : numbers)
+    {
+        for (auto board : boards)
+        {
+            board->mark(num);
+            
+            if (board->win())
+                boardsLeft--;
+            else
+                losingBoard = board;
+        }
+        
+        if (boardsLeft == 0)
+        {
+            lastNum = num;
+            break;
+        }
+        else
+        {
+            boardsLeft = boardCount;
+            continue;
+        }
+    }
+        
+    std::cout << "Sum of unmarked numbers is " << losingBoard->unmarkedSum() << std::endl
+              << "The last number was " << lastNum << std::endl
+              << "The result is " << losingBoard->unmarkedSum() * lastNum << std::endl;
+    
+    for (auto b : boards)
+        delete b;
+    
+    return 0;
+}
