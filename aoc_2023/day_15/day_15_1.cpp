c6d0115cde48b2b90756cd1b5d8f7b1df74b4963
Jul-Le <julius.lesonen@gmail.com>
2023-12-16 11:13:07 +0200

    Day 15 part 1 update description

--- aoc_2023/day_15/day_15_1.cpp
+++ aoc_2023/day_15/day_15_1.cpp
@@ -6,7 +6,14 @@
  *
  *
  *
- *  Description
+ *  Input is a csv of strings. HASH algorithm works as follows:
+ *
+ *  Determine the ASCII code for the current character of the string.
+ *  Increase the current value by the ASCII code you just determined.
+ *  Set the current value to itself multiplied by 17.
+ *  Set the current value to the remainder of dividing itself by 256.
+ *
+ *  What is the sum of the HASH codes of all the strings?
  */
 
 #include <iostream>
